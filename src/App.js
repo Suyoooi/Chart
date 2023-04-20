541aebbc2bfc44a4b17582d887a95085cbca3877
권수연 <yeoniii20@epozen.com>
2023-04-20 13:53:15 +0900

    feat: D3chart 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import D3Chart from "./constant/D3Chart";
 import Main from "./constant/Main";
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <D3Chart />
     </div>
   );
 }
