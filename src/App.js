4950b3a50ac0c11c4a19646e2c5caaadba473b8a
권수연 <yeoniii20@epozen.com>
2023-04-20 18:03:17 +0900

    feat: react-chart 생성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Main from "./constant/Main";
+import ReactChart from "./constant/React-Chart-2";
+import ReactChartTest from "./constant/React-Chart-2-Test";
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <ReactChart />
+      <ReactChartTest />
     </div>
   );
 }
