a7e6904c0094fed7be56bf86037834a0ad5c77bf
권수연 <yeoniii20@epozen.com>
2023-04-20 13:38:04 +0900

    fix: echartfor app.js에 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Echartfor from "./constant/EChartFor";
 import Main from "./constant/Main";
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <Echartfor />
     </div>
   );
 }
