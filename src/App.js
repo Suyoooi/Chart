a28d6d745add51105e49a33497741700f65214e4
권수연 <yeoniii20@epozen.com>
2023-04-21 16:05:23 +0900

    feat: Page에 D3 Chart만 표시

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import "./App.css";
-import Echartfor from "./constant/EChartFor";
 import D3Chart from "./constant/D3Chart";
 import Main from "./constant/Main";
-import ReactChart from "./constant/React-Chart-2";
-import ReactChartTest from "./constant/React-Chart-2-Test";
 
 function App() {
   return (
     <div className="App">
       <Main />
-      <Echartfor />
-      <ReactChart />
-      <ReactChartTest />
       <D3Chart />
     </div>
   );
