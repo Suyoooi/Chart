690bba910d3e44adc4256b610f84160ff2647999
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-08 13:14:45 +0900

    fix: 받아오는 data 수 줄이기

--- src/constant/React-Chart-2-Test/React-Chart-2-Test.jsx
+++ src/constant/React-Chart-2-Test/React-Chart-2-Test.jsx
@@ -4,13 +4,13 @@ import { Line } from "react-chartjs-2";
 
 //y축 데이터 생성
 const arr = [];
-for (let i = 0; i < 3000; i++) {
+for (let i = 0; i < 100; i++) {
   arr.push(Math.floor(Math.random() * 100) + 1);
   console.log(arr);
 }
 //x축 데이터 생성
 var array = [];
-for (var i = 0; i < 3000; i++) {
+for (var i = 0; i < 100; i++) {
   array.push("test");
 }
 console.log(array);
@@ -31,8 +31,8 @@ const data = {
 const ReactChartTest = () => {
   return (
     <Container>
-      <h1>React-Chart-2 1007개 data</h1>
-      <p>x축 label 63 개</p>
+      <h1>React-Chart-2 data</h1>
+      <p>x축 label</p>
       <Line data={data} />
     </Container>
   );
