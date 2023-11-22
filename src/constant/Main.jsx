e09e7dd3cf739f1f2eb243af0e2c8a11e02b01d7
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-11-22 12:39:21 +0900

    feat: Multi D3Chart Main 컴포넌트에 추가

--- src/constant/Main.jsx
+++ src/constant/Main.jsx
@@ -1,5 +1,12 @@
+import D3ChartMulti from "./D3Chart/D3ChartMulti";
+import { data } from "./D3Chart/data";
+
 const Main = () => {
-  return <div>Chart Test</div>;
+  return (
+    <div>
+      <D3ChartMulti data={data} />
+    </div>
+  );
 };
 
 export default Main;
